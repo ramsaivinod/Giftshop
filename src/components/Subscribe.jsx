8c9a5907b3d9391dfcc5d7dc69d21a1baf13392c
Satya Prakash <satya.prakash@jaiinfoway.com>
2023-09-07 19:06:57 +0530

    footer res

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -17,56 +17,64 @@ const Subscribe = () => {
   const breakPoint = useMediaQuery("(max-width:500px)");
 
   return (
-    <Box width="80%" margin="30px auto" textAlign="center">
-      <IconButton>
-        <MarkEmailReadOutlinedIcon
-          fontSize="large"
-          style={{ color: "#ff6d2f" }}
-        />
-      </IconButton>
-      <Typography variant="h2" color="#ff6d2f">
-        Subscribe To Our Newsletter
-      </Typography>
-      <Typography color="white">
-        Get all the latest news, upcoming programs, retreats and other updates
-        regularly!
-      </Typography>
-      <Box
-        p="2px 4px"
-        m="15px auto"
-        display="flex"
-        alignItems="center"
-        width= {breakPoint ? "100%" : "55%"} 
-        backgroundColor="#F2F2F2"
+    <Box className="subscribe" >
+      <div className="row"
       >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Enter Name"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <div className="col-md-4 col-sm-12 textleft">
 
-        <Button>
-          {" "}
-          <Typography
-            sx={{
-              p: "10px",
-              ":hover": { cursor: "pointer" },
-              background: "#ff6d2f",
-            }}
-          >
-         Subscribe
-          </Typography>
-        </Button>
-      </Box>
+          <IconButton>
+            <MarkEmailReadOutlinedIcon
+              fontSize="medium"
+              style={{ color: "#ff6d2f" }}
+            />
+          </IconButton>
+          <div> <p className="txt">
+            Subscribe To Our Newsletter
+          </p>
+            <p className="desc">
+              Get all the latest news, upcoming programs, retreats and other updates
+              regularly!
+            </p></div>
+        </div>
+        <div className="col-md-8 col-sm-12">  <Box
+          p="2px 4px"
+          m="15px auto"
+          display="flex"
+          alignItems="center"
+          width={breakPoint ? "100%" : "55%"}
+          backgroundColor="#F2F2F2"
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+          <Button>
+            {" "}
+            <Typography
+              sx={{
+                p: "10px",
+                ":hover": { cursor: "pointer" },
+                background: "#ff6d2f",
+              }}
+            >
+              Subscribe
+            </Typography>
+          </Button>
+        </Box></div>
+      </div>
+
+
     </Box>
   );
 };
