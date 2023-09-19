02a96e00d465671d86d7364605261eaa2e1a10a7
chitranjan <chitranjan.it@gmail.com>
2023-09-19 13:21:44 +0530

    update the banner code and add the comment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,143 +1,95 @@
-import { useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Typography,
-  Grid,
-  IconButton,
-  Container,
-  Icon,
-  useTheme,
-  Button,
-} from "@mui/material"
-import { ShoppingCart, LocalShipping, Payment } from "@mui/icons-material"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import CloseIcon from "@mui/icons-material/Close"
-import AddIcon from "@mui/icons-material/Add"
-import RemoveIcon from "@mui/icons-material/Remove"
-import MoneyIcon from "@mui/icons-material/Money"
-import { shades, theme } from "../theme"
-import { makeStyles } from "tss-react/mui"
-import top100 from "../logo/top100.png"
-import audiobooks from "../logo/JKYOG-Academy-compressed.jpeg"
-import books from "../logo/Purpose-of-Life-min.jpeg"
-import coupons from "../logo/SMEX-Thumbnail-min.jpeg"
-import giftcard from "../logo/SMLA-min.jpeg"
-import pen from "../logo/Yoga.jpeg"
-import trophy from "../logo/5.webp"
-import { fetchDataFromApi } from "../utils/api";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; // Importing the slick carousel CSS
+import 'slick-carousel/slick/slick-theme.css'; // Importing the slick carousel theme CSS
 
+import { fetchDataFromApi } from '../utils/api'; // Importing a function to fetch data from an API
 
-import useMediaQuery from "@mui/material/useMediaQuery"
-import giftcard2 from "../logo/giftcard.svg"
-import "../styles/banner.css"
-import { useNavigate } from "react-router-dom"
-import { Fragment } from "react"
+import '../styles/banner.css'; // Importing CSS styles for the banner
 
-
-//books,coupons,giftcard,pen,trophy
-const pics = [
-  { i: audiobooks, c: "Bestsellers" },
-  { i: books, c: "Books" },
-  { i: coupons, c: "Coupons" },
-  { i: giftcard, c: "Giftcard" },
-  { i: pen, c: "Pen" },
-  { i: trophy, c: "Trophy" },
-]
-
-const picsList = [audiobooks,books,coupons,giftcard,pen,trophy,audiobooks,books,giftcard,coupons,giftcard,pen,trophy];
-
+// Slick carousel settings
 var settings = {
-  dots: false,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 6,
-  slidesToScroll: 2,
-  initialSlide: 0,
+  dots: false, // Display navigation dots (pagination) as indicators
+  infinite: false, // Allow infinite loop of carousel items
+  speed: 500, // Transition speed in milliseconds
+  slidesToShow: 6, // Number of items to show at once
+  slidesToScroll: 2, // Number of items to scroll at a time
+  initialSlide: 0, // Initial slide index
 
   responsive: [
-
     {
-      breakpoint: 1024,
+      breakpoint: 1024, // Screen width breakpoint
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-
-
+        slidesToShow: 4, // Adjusted number of items to show
+        slidesToScroll: 1, // Adjusted number of items to scroll
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 600, // Screen width breakpoint
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-
+        slidesToShow: 4, // Adjusted number of items to show
+        slidesToScroll: 1, // Adjusted number of items to scroll
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 480, // Screen width breakpoint
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-
+        slidesToShow: 4, // Adjusted number of items to show
+        slidesToScroll: 1, // Adjusted number of items to scroll
       },
     },
   ],
-}
+};
+
 function Banner() {
-  const [categoryList,setCategoryList] = useState([]);
-  const [tags,setTags] = useState([]);
-  const breakPoint = useMediaQuery("(min-width:870px)")
-  const navigate = useNavigate()
-  const itemsCategories = useSelector((state) => state.cart.itemsCategories);
+  const [tags, setTags] = useState([]); // State variable to store fetched data
+  const navigate = useNavigate(); // Access the router's navigation function
 
-  useEffect(()=>{
-    setCategoryList(itemsCategories);
-  },[itemsCategories])
-
-
-  useEffect(()=>{
-    const getTags = async() => {
+  useEffect(() => {
+    // Fetch data from an API when the component mounts
+    const getTags = async () => {
       try {
-        const  resp = await fetchDataFromApi("/api/gift-shop-contents?populate=*");
-        if(resp){
+        // Call the fetchDataFromApi function to fetch data
+        const resp = await fetchDataFromApi('/api/gift-shop-contents?populate=*');
+        if (resp) {
+          // Update the 'tags' state with the fetched data
           setTags(resp?.data);
         }
       } catch (error) {
-        console.error("Error fetching blogs:", error);
+        console.error('Error fetching blogs:', error);
       }
-      
     };
-    getTags()
-  },[])
+    getTags(); // Call the getTags function to initiate the data fetching
+  }, []);
 
   return (
-
-
     <div className="banner_box">
-      {tags?.length ? 
-        <Slider {...settings} style={{ width: "100%" }}>
-          {tags?.map((item,i) => {
+      {tags?.length ? ( // Check if there are tags available in the state
+        <Slider {...settings} style={{ width: '100%' }}>
+          {tags?.map((item, i) => {
             return (
-              <div key={i} className="banner_icon" onClick={() => navigate(`/category/${item?.attributes?.title}`)} >
+              <div key={i} className="banner_icon" onClick={() => navigate(`/category/${item?.attributes?.title}`)}>
                 <div className="bannerimg">
-                  <img src={item?.attributes?.content_image?.data?.attributes?.url} alt="banner-image" className="banner_img" />
+                  <img
+                    src={item?.attributes?.content_image?.data?.attributes?.url}
+                    alt="banner-image"
+                    className="banner_img"
+                  />
                 </div>
 
                 <div className="banner_text">
                   <div className="text">{item?.attributes?.title}</div>
                 </div>
               </div>
-            )
+            );
           })}
-      </Slider>
-      : <></>
-      }
-      
+        </Slider>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
