ae7f99eede752dd1dc35cee5da95c6307b89cedc
chitranjan <chitranjan.it@gmail.com>
2023-09-19 12:12:55 +0530

    Card page code clean and commented

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -1,40 +1,37 @@
-import { Box, Button, Divider, IconButton, Typography } from "@mui/material"
-import { useSelector, useDispatch } from "react-redux"
-import CloseIcon from "@mui/icons-material/Close"
-import AddIcon from "@mui/icons-material/Add"
-import RemoveIcon from "@mui/icons-material/Remove"
-import styled from "@emotion/styled"
-import { shades } from "../theme"
+// Import necessary components and styles
+import { Box, Button, Divider, IconButton, Typography } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
 
-import {
-  decreaseCount,
-  increaseCount,
-  removeFromCart,
-  setIsCartOpen,
-} from "../state"
-import { useNavigate } from "react-router-dom"
+import { shades } from '../theme';
+import { decreaseCount, increaseCount, removeFromCart, setIsCartOpen } from '../state';
 
+// Create a styled component for a flex container
 const FlexBox = styled(Box)`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+`;
 
 const CartMenu = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const cart = useSelector((state) => state.cart.cart)
-  const isCartOpen = useSelector((state) => state.cart.isCartOpen)
+  // Define variables and hooks
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
 
+  // Calculate the total price of items in the cart
   const totalPrice = cart.reduce((total, item) => {
-    return total + item.count * item.variants[0].price
-  }, 0)
-
-  console.log(cart, "this is cart")
+    return total + item.count * item.variants[0].price;
+  }, 0);
 
   return (
     <Box
-      display={isCartOpen ? "block" : "none"}
+      display={isCartOpen ? 'block' : 'none'}
       backgroundColor="rgba(0, 0, 0, 0.4)"
       position="fixed"
       zIndex={99}
@@ -42,15 +39,8 @@ const CartMenu = () => {
       height="100%"
       left="0"
       top="0"
-      overflow="auto"
-    >
-      <Box className="cart-page"
-        position="fixed"
-        right="0"
-        bottom="0"
-        height="100%"
-        backgroundColor="white"
-      >
+      overflow="auto">
+      <Box className="cart-page" position="fixed" right="0" bottom="0" height="100%" backgroundColor="white">
         <Box overflow="auto" height="100%" className="full-cart-page">
           {/* HEADER */}
           <FlexBox mb="15px">
@@ -66,50 +56,26 @@ const CartMenu = () => {
               <Box key={`${item.id}`}>
                 <FlexBox p="15px 0">
                   <Box className="flex-image" flex="1 1 40%">
-                    <img
-                      alt={item?.name}
-                      width="123px"
-                      height="164px"
-                      src={item.image?.src}
-                    />
+                    <img alt={item?.name} width="123px" height="164px" src={item.image?.src} />
                   </Box>
                   <Box flex="1 1 60%">
                     <FlexBox mb="5px">
                       <Typography fontWeight="bold">{item.title}</Typography>
-                      <IconButton
-                        onClick={() =>
-                          dispatch(removeFromCart({ id: item.id }))
-                        }
-                      >
+                      <IconButton onClick={() => dispatch(removeFromCart({ id: item.id }))}>
                         <CloseIcon />
                       </IconButton>
                     </FlexBox>
-                    {/* <Typography>{item.attributes.shortDescription}</Typography> */}
                     <FlexBox m="15px 0">
-                      <Box
-                        display="flex"
-                        alignItems="center"
-                        border={`1.5px solid ${shades.neutral[500]}`}
-                      >
-                        <IconButton
-                          onClick={() =>
-                            dispatch(decreaseCount({ id: item.id }))
-                          }
-                        >
+                      <Box display="flex" alignItems="center" border={`1.5px solid ${shades.neutral[500]}`}>
+                        <IconButton onClick={() => dispatch(decreaseCount({ id: item.id }))}>
                           <RemoveIcon />
                         </IconButton>
                         <Typography>{item.count}</Typography>
-                        <IconButton
-                          onClick={() =>
-                            dispatch(increaseCount({ id: item.id }))
-                          }
-                        >
+                        <IconButton onClick={() => dispatch(increaseCount({ id: item.id }))}>
                           <AddIcon />
                         </IconButton>
                       </Box>
-                      <Typography fontWeight="bold">
-                        ${item.variants[0].price}
-                      </Typography>
+                      <Typography fontWeight="bold">${item.variants[0].price}</Typography>
                     </FlexBox>
                   </Box>
                 </FlexBox>
@@ -128,29 +94,28 @@ const CartMenu = () => {
               disabled={cart.length === 0}
               sx={{
                 backgroundColor: shades.primary[400],
-                color: "white",
+                color: 'white',
                 borderRadius: 0,
-                minWidth: "100%",
-                padding: "20px 40px",
-                m: "20px 0",
-                display: cart.length === 0 ? "none" : "",
-                "&:hover": {
-                  backgroundColor: "#ff6d31",
+                minWidth: '100%',
+                padding: '20px 40px',
+                m: '20px 0',
+                display: cart.length === 0 ? 'none' : '',
+                '&:hover': {
+                  backgroundColor: '#ff6d31',
                 },
               }}
               onClick={() => {
-                navigate("/checkout")
-                dispatch(setIsCartOpen({}))
-                window.scrollTo(0, 0)
-              }}
-            >
+                navigate('/checkout');
+                dispatch(setIsCartOpen({}));
+                window.scrollTo(0, 0);
+              }}>
               CHECKOUT
             </Button>
           </Box>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default CartMenu
+export default CartMenu;
