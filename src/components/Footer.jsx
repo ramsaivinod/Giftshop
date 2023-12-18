c0123ec54da139d78026949dfefae66a5120abb0
chitranjan <chitranjan.mukhiya@jaiinfoway.com>
2023-12-18 08:27:03 +0530

    footer sections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,199 +1,311 @@
 // Import necessary modules, components, and icons
-import { Box, Typography, IconButton } from '@mui/material';
-import images from '../assets/images/main-footer-bg.jpg';
-import menu from '../assets/images/menu-shape.png';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import HomeIcon from '@mui/icons-material/Home';
-import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import '../styles/footer.css';
+import { Box, Typography, IconButton } from "@mui/material";
+import images from "../assets/images/main-footer-bg.jpg";
+import menu from "../assets/images/menu-shape.png";
+import appStore from "../assets/images/App-Store-Logo.png";
+import krishnaBhakti from "../assets/images/logo.png";
+import whatsapp from "../assets/images/whatsapp.png";
+import mail from "../assets/images/mail.png";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import "../styles/footer.css";
 
 function Footer() {
   return (
-    <Box backgroundColor="#171381" style={{ backgroundImage: `url(${images})` }}>
+    <Box
+      backgroundColor="#171381"
+      style={{ backgroundImage: `url(${images})` }}
+    >
       <Box className="footerbox">
         {/* Commented out Subscribe component */}
         {/* <Subscribe /> */}
-        <div className="row content" style={{ marginTop: '50px' }}>
-          <div className="col-sm-12 col-md-5">
-            <Box className="full-address">
-              <Typography className="htxt">Contact Us</Typography>
-              {/* Contact information */}
-              <Typography color="white" style={{ fontSize: '16px', lineHeight: '24px', marginBottom: '10px' }}>
-                <MailOutlinedIcon style={{ position: 'relative', top: '-2px', marginRight: '10px' }} />
-                Email: secretary@jkyog.org
-              </Typography>
-              <Typography
-                sx={{ wordWrap: 'break-word' }}
-                color="white"
-                style={{ fontSize: '16px', lineHeight: '24px', marginBottom: '10px' }}>
-                <HomeIcon style={{ position: 'relative', top: '-2px', marginRight: '10px' }} />
-                Plano, Tx 75025 U.S
-              </Typography>
-              <Typography color="white" style={{ fontSize: '16px', lineHeight: '24px', marginBottom: '10px' }}>
-                <LocalPhoneIcon style={{ position: 'relative', top: '-2px', marginRight: '10px' }} />
-                +1 (469) 795-9135
-              </Typography>
-            </Box>
-            {/* Links for donation, volunteering, and login */}
+        <div className="row content" style={{ marginTop: "50px" }}>
+          <div className="col-sm-12 col-md-3">
             <div className="donate-login-vol">
-              <ul>
+              <img
+                src={appStore}
+                alt="appStore"
+                style={{ height: "4rem" }}
+                title="appStore"
+              />
+            </div>
+            <div className="donate-login-vol">
+              <img
+                src={krishnaBhakti}
+                style={{ height: "4rem" }}
+                alt="krishnaBhakti"
+                title="krishnaBhakti"
+              />
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-3 programs-center">
+            <Box>
+              <Typography className="htxt">Categories</Typography>
+              {/* List of center links */}
+              <ul style={{ color: "#EF6F1F" }}>
                 <li>
-                  <a href="https://jkyog.org/donate" target="_blank" title="Donate Page Url">
-                    <img
-                      src="https://www.jkyog.org/themes/custom/jkyog/images/donate.png"
-                      alt="Donate"
-                      title="Donate"
-                    />
-                    <br />
-                    Donate
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Life Transformation
+                    </Typography>
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.jkyog.org/volunteer" target="_self" title="Volunteer Page Url ">
-                    <img
-                      src="https://www.jkyog.org/themes/custom/jkyog/images/volunteer.png"
-                      alt="Volunteer"
-                      title="Volunteer"
-                    />
-                    <br />
-                    Volunteer
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Yoga & Lifestyle
+                    </Typography>
                   </a>
                 </li>
                 <li>
-                  <a href="https://login.jkyog.org/user/login" title="Login Page Url" style={{ lineHeight: '25px' }}>
-                    <img src="https://www.jkyog.org/themes/custom/jkyog/images/login.png" alt="Login" title="Login" />{' '}
-                    <br />
-                    Login
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Best Sellers - tag-driven
+                    </Typography>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Signed Editions
+                    </Typography>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Audiobooks
+                    </Typography>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.jkyog.org/centers"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
+                      Self-Help
+                    </Typography>
                   </a>
                 </li>
               </ul>
-            </div>
+            </Box>
           </div>
           <div className="col-sm-12 col-md-3 programs-center">
             <Box>
               <Typography className="htxt">Programs</Typography>
               {/* List of program links */}
-              <ul style={{ color: '#EF6F1F' }}>
+              <ul style={{ color: "#EF6F1F" }}>
                 <li>
-                  <a href="https://www.jkyog.org/life-transformation-programs" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
+                  <a
+                    href="https://www.jkyog.org/life-transformation-programs"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
                       Life Transformation Program
                     </Typography>
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.jkyog.org/retreats" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
+                  <a
+                    href="https://www.jkyog.org/retreats"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
                       JKYOG Retreats
                     </Typography>
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.jkyog.org/bal-mukund" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
+                  <a
+                    href="https://www.jkyog.org/bal-mukund"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
                       Bal-Mukund
                     </Typography>
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.jkyog.org/jkyog-youth-club" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
+                  <a
+                    href="https://www.jkyog.org/jkyog-youth-club"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography color="white" textAlign={"left"}>
+                      {" "}
                       JKYog Youth Club
                     </Typography>
                   </a>
                 </li>
               </ul>
             </Box>
-            <Box>
-              <Typography className="htxt">Our Centers</Typography>
-              {/* List of center links */}
-              <ul style={{ color: '#EF6F1F' }}>
-                <li>
-                  <a href="https://www.jkyog.org/centers" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
-                      USA
-                    </Typography>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.jkyog.org/centers" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
-                      INDIA
-                    </Typography>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.jkyog.org/centers" target="_blank" rel="noreferrer">
-                    <Typography color="white" textAlign={'left'}>
-                      {' '}
-                      SINGAPORE
-                    </Typography>
-                  </a>
-                </li>
-              </ul>
-            </Box>
           </div>
-          <div className="col-sm-12 col-md-4">
+          <div className="col-sm-12 col-md-3" style={{ display:"grid" }}>
             <Box>
-              <Typography className="htxt" color="white">
-                Connect With US
+              <h2 className="htxt" style={{ fontWeight: "400" }}>
+                Help Desk Contact
+              </h2>
+              <Typography>
+                <div className="whatsapp">
+                  <a href="tel:+5555555555" style={{ display: 'flex', textDecoration: 'none', color: '#fff', alignItems: 'center', }}>
+                    <img
+                      src={whatsapp}
+                      alt="whatsapp"
+                      style={{ height: "1.2rem" }}
+                      title="whatsapp"
+                    />
+                    <span style={{ padding: "0 1rem" }}>+555-555-5555</span>
+                  </a>
+                </div>
+                <div className="mail">
+                  <a href="mailto:info@jkyog.org" style={{ display: 'flex', textDecoration: 'none', color: '#fff', alignItems: 'center', }}>
+                    <img
+                      src={mail}
+                      alt="mail"
+                      style={{ height: "1.2rem" }}
+                      title="mail"
+                    />
+                    <span style={{ padding: "0 1rem" }}>info@jkyog.org</span>
+                  </a>
+                </div>
               </Typography>
+            </Box>
+            <Box style={{ marginTop:"2rem" }}>
+              <Typography className="htxt">Connect With US</Typography>
               {/* Social media icons and links */}
-              <Box display={'flex'}>
+              <Box display={"flex"}>
                 <Box>
                   <IconButton>
-                    <a href="https://www.youtube.com/channel/UCclfz6zVWWOpsQsg3OheI3g" target="_blank" rel="noreferrer">
-                      {' '}
-                      <YouTubeIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                    <a
+                      href="https://www.youtube.com/channel/UCclfz6zVWWOpsQsg3OheI3g"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {" "}
+                      <YouTubeIcon
+                        fontSize="large"
+                        style={{ color: "#EF6F1F" }}
+                      />
                     </a>
                   </IconButton>
                 </Box>
                 <IconButton>
-                  <a href="https://www.facebook.com/Swami.Mukundananda/" target="_blank" rel="noreferrer">
-                    <FacebookIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://www.facebook.com/Swami.Mukundananda/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FacebookIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
                 <IconButton>
-                  <a href="https://twitter.com/Sw_Mukundananda" target="_blank" rel="noreferrer">
-                    {' '}
-                    <TwitterIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://twitter.com/Sw_Mukundananda"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <TwitterIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
                 <IconButton>
-                  <a href="https://chat.whatsapp.com/HwyzZcNFJAUIESvu9VwFOU" target="_blank" rel="noreferrer">
-                    {' '}
-                    <WhatsAppIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://chat.whatsapp.com/HwyzZcNFJAUIESvu9VwFOU"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <WhatsAppIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
                 <IconButton>
-                  <a href="https://www.linkedin.com/in/swamimukundananda/" target="_blank" rel="noreferrer">
-                    {' '}
-                    <LinkedInIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://www.linkedin.com/in/swamimukundananda/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <LinkedInIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
                 <IconButton>
-                  <a href="https://www.instagram.com/swami_mukundananda/?hl=en" target="_blank" rel="noreferrer">
-                    {' '}
-                    <InstagramIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://www.instagram.com/swami_mukundananda/?hl=en"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <InstagramIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
                 <IconButton>
-                  <a href="https://www.pinterest.com/SwamiMukundananda/" target="_blank" rel="noreferrer">
-                    {' '}
-                    <PinterestIcon fontSize="large" style={{ color: '#EF6F1F' }} />
+                  <a
+                    href="https://www.pinterest.com/SwamiMukundananda/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <PinterestIcon
+                      fontSize="large"
+                      style={{ color: "#EF6F1F" }}
+                    />
                   </a>
                 </IconButton>
               </Box>
@@ -202,14 +314,15 @@ function Footer() {
                 <Typography
                   varinat="h4"
                   fontSize="medium"
-                  fontWeight={'600'}
+                  fontWeight={"600"}
                   style={{
                     backgroundImage: `url(${menu})`,
-                    height: '55px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    backgroundRepeat: 'no-repeat',
-                  }}>
+                    height: "55px",
+                    display: "flex",
+                    alignItems: "center",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                >
                   <span className="ms-4"> 5M Followers</span>
                 </Typography>
               </Box>
@@ -220,7 +333,9 @@ function Footer() {
       {/* Mini footer section */}
       <section className="mini-footer">
         <div className="content-area">
-          <div className="copy-right">© Copyright 2020 JKYog. All Rights Reserved.</div>
+          <div className="copy-right">
+            © Copyright 2020 JKYog. All Rights Reserved.
+          </div>
           <div className="terms-policy">
             <ul>
               {/* Links to terms and conditions and privacy policy */}
