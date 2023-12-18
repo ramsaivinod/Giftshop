dcedef18a452cb074cd177ea4e0b7c50e13de04e
chitranjan <chitranjan.mukhiya@jaiinfoway.com>
2023-12-18 08:18:07 +0530

    nesletter functions

--- src/components/StayConnected.jsx
+++ src/components/StayConnected.jsx
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
 import './StayConnected.css'; // Import the CSS file for styling
+import axios from "axios";
 
 const StayConnected = () => {
   const [email, setEmail] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const apiKey = '961d0f182b31ae37dc63e644d4178641';
+    const password = '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'; // API secret key can be used here
+    const shopName = 'jkyoggiftshop';
+    const your_access_token = "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";
+
+    // https://${apiKey}:${password}@${shopName}.myshopify.com/admin/api/2023-07/customers.json
+
+    const url = `https://${apiKey}:${password}@${shopName}.myshopify.com/admin/api/2023-07/customers.json`;
+    const payload = {
+        customer: {
+          email: email,
+        }
+      };
+
+    axios.post(url, payload, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Access-Token': your_access_token,
+        }
+      })
+      .then(response => console.log(response.data))
+      .catch(error => console.error(error));
+
     // Implement what happens when the form is submitted
     console.log('Email submitted:', email);
   };
